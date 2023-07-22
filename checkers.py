9fcefc63a84119192278cc93a4773ec30f232425
JPablix <j.granados@estudiantec.cr>
2023-07-22 01:01:49 -0600

    FIX: crear_tablero now use O to black spaces and X to white spaces.

--- checkers.py
+++ checkers.py
@@ -1,16 +1,36 @@
-#Sample of a mini chekers with text interface
+#Sample of a mini checkers with text interface
+
+#Apuntes: Fichas blancas: W, fichas negras: B, casillas vacias: O, reinas blancas: Q, reinas negras: K
+#         
 
 tablero = []
 
 def crear_tablero():
-    for i in range(8):
-        tablero.append(["O"] * 8)
+    i = 0       
+    while i <= 8:           #Creación de filas
+        tablero.append([])
+        j = 0
+
+        if i%2 == 0:        #Saber con qué elemento empezar
+            espacio = "O"
+        else:
+            espacio = "X"
+
+        while j <= 8:       #Creación de columnas
+            tablero[i].append(espacio)
+            if espacio == "O":
+                espacio = "X"
+            else:
+                espacio = "O"
+            j += 1
+        i += 1
 
 def imprimir_tablero(tablero):
     for fila in tablero:
         print("")
         for elemento in fila:
             print(elemento, end=" ")
+
 
 crear_tablero()
 imprimir_tablero(tablero)
